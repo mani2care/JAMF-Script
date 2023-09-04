6264d47227a828b7640ab988312d2b40d16ae59c
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-09-04 12:45:37 +0530

    Local Administrative Rights Remove.bash

---
+++ Administrator/Local Administrative Rights Remove.bash
@@ -0,0 +1,105 @@
+#!/bin/bash
+####################################################################################################
+#
+# ABOUT
+#
+#	Removes admin rights from adminstrators NOT specificially listed
+#
+####################################################################################################
+#
+# HISTORY
+# Global Variables
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Script Version and Jamf Pro Script Parameters
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+scriptVersion="1.0.3"
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+scriptLog="${4:-"/var/log/org.churchofjesuschrist.log"}"    # Parameter 4: Script Log Location
+approvedAdmins=(root _hiddenLocalAdmin localadmin1)         # Space-delimited list of approved local admins
+adminUsers=$( /usr/bin/dscl . -read Groups/admin GroupMembership | /usr/bin/cut -c 18- )
+adminArray=($adminUsers)
+
+####################################################################################################
+#
+# Pre-flight Checks
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Client-side Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ ! -f "${scriptLog}" ]]; then
+    touch "${scriptLog}"
+fi
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Client-side Script Logging Function
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function updateScriptLog() {
+    echo -e "$( date +%Y-%m-%d\ %H:%M:%S ) - ${1}" | tee -a "${scriptLog}"
+}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Logging Preamble
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+updateScriptLog "\n\n###\n# Local Administrative Rights Remove (${scriptVersion})\n###\n"
+updateScriptLog "PRE-FLIGHT CHECK: Initiating …"
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Confirm script is running as root
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ $(id -u) -ne 0 ]]; then
+    updateScriptLog "PRE-FLIGHT CHECK: This script must be run as root; exiting."
+    exit 1
+fi
+
+####################################################################################################
+#
+# Functions
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Check for approved admins
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function adminCheck() {
+    adminAccountToCheck="${1}"
+    if [[ " ${approvedAdmins[*]} " =~ " ${adminAccountToCheck} " ]]; then
+        updateScriptLog "Approved admin '${adminAccountToCheck}' unchanged"
+    else
+        dseditgroup -o edit -d "${adminAccountToCheck}" -t user admin
+        if [[ $? = 0 ]]; then updateScriptLog "Removed user '${adminAccountToCheck}' from admin group"; fi
+    fi
+}
+
+####################################################################################################
+#
+# Program
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Remove Local Administrative Rights
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+for adminAccount in ${adminArray[@]}; do
+    adminCheck "${adminAccount}"
+done
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Exit
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+updateScriptLog "Goodbye!"
+
+exit 0
