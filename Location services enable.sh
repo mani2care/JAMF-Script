2aa270b4da743c55b065120e13948c99826e593c
Manikandan R <52978899+mani2care@users.noreply.github.com>
2023-12-19 11:48:45 +0530

    Create Location services enable.sh

---
+++ Location services enable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## enabling location services
+/usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -int 1
+
+uuid=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Hardware UUID" | cut -c22-57)
+/usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd.$uuid LocationServicesEnabled -int 1
+
+/bin/launchctl kickstart -k system/com.apple.locationd 2>&1
+
+exit 0
