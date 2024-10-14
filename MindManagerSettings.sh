b14ec2cc68bdcbd5274d687580bd6ee355eb9dfd
Manikandan R <52978899+mani2care@users.noreply.github.com>
2024-10-14 21:54:32 +0530

    Create MindManagerSettings.sh

---
+++ MindManagerSettings.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# MindManagerSettings.sh -- Set License and accept EULA
+
+#---Variables---#
+currentuser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+echo "Logged in user: $currentuser"
+
+
+#---LicenseKey and Settings---#
+#su -l $currentuser -c "defaults -currentHost write com.mindjet.mindmanager.23 LicenseKey 'APxxxx-xxxx-xxxx-xxxx'"
+
+#---Functions---#
+
+apply_mindmanager_23_settings() {
+    echo "Applying settings for MindManager 23..."
+    su -l "$currentuser" -c "defaults -currentHost write com.mindjet.mindmanager.23 ShowEULA -int 0"
+    su -l "$currentuser" -c "defaults -currentHost write com.mindjet.mindmanager.23 Edition -int 0"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.23 FirstLaunch -bool false"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.23 DeviceAutoUpdateDisabled -bool false"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.23 StartupDialogPolicy -int 3"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.23 NewDocumentOnStart -int 1"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 ShowCheckForUpdates -int 0"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 UpdateCheck -int 86400"
+}
+
+apply_mindmanager_24_settings() {
+    echo "Applying settings for MindManager 24..."
+    rm -rf /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.23.plist
+    su -l "$currentuser" -c "defaults -currentHost write com.mindjet.mindmanager.24 ShowEULA -int 0"
+    su -l "$currentuser" -c "defaults -currentHost write com.mindjet.mindmanager.24 Edition -int 0"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 FirstLaunch -bool false"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 DeviceAutoUpdateDisabled -bool false"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 StartupDialogPolicy -int 3"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 NewDocumentOnStart -int 1"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 ShowCheckForUpdates -int 0"
+    su -l "$currentuser" -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.24 UpdateCheck -int 86400"
+}
+
+
+#---Check MindManager version from Info.plist---#
+
+plist_path="/Applications/MindManager.app/Contents/Info.plist"
+
+if [ -f "$plist_path" ]; then
+    version=$(defaults read "$plist_path" CFBundleShortVersionString)
+    echo "Detected MindManager version: $version"
+
+    if [[ $version == 23.* ]]; then
+        apply_mindmanager_23_settings
+    elif [[ $version == 24.* ]]; then
+        apply_mindmanager_24_settings
+    else
+        echo "Installed MindManager version is not supported."
+    fi
+else
+    echo "MindManager is not installed."
+fi
+
+echo "Settings have been applied successfully."
