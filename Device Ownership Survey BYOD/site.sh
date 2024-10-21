fd8929f4c367974192a5cceb0d8f6cc5a724f8e3
Manikandan R <52978899+mani2care@users.noreply.github.com>
2024-10-21 16:07:13 +0530

    Update site.sh

--- Device Ownership Survey BYOD/site.sh
+++ Device Ownership Survey BYOD/site.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-siteName=$(profiles -P -o stdout | grep -i -A 20 "User Info" | grep "site_name" | awk -F'=' '{gsub(/^ *| *$/,"",$2); print $2}' | tr -d ';' 2>/dev/null)
-echo $siteName
-if [[ $siteName ]]; then
-   echo "<result>${siteName}</result>"
+# Check if the username is present in /Library/Managed Preferences/com.abb.deviceinfo.plist
+siteName=$(defaults read "/Library/Managed Preferences/com.abb.deviceinfo.plist" site_name 2>/dev/null)
+
+if [ -n "$username" ]; then
+    echo "<result>${siteName}</result>"
 else
-   echo "<result>Not_Available</result>"
+    # If not found in deviceinfo.plist, check using profiles command
+    siteName=$(profiles -P -o stdout | grep -i -A 20 "User Info" | grep "site_name" | awk -F'=' '{gsub(/^ *| *$/,"",$2); print $2}' | tr -d ';' | tr -d '"' 2>/dev/null)
+    
+    if [ -n "$siteName" ]; then
+        echo "<result>${siteName}</result>"
+    else
+        echo "<result>Not_Available</result>"
+    fi
 fi
